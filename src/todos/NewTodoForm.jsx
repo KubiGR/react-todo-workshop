38d7ca7afe240a7039168e1d99b36a74cd8aaa1d
Andreas Dranidis <adranidis@trigonongroup.com>
2022-05-27 16:13:17 +0300

    Implement half branch

--- src/todos/NewTodoForm.jsx
+++ src/todos/NewTodoForm.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import "./NewTodoForm.css";
 
-const NewTodoForm = ({ addTodo }) => {
-  const [inputValue, setInputValue] = useState("");
+const NewTodoForm = () => {
+  // const [inputValue, setInputValue] = useState("");
 
-  const createTodo = () => {
-    if (inputValue === "") return;
-    addTodo({ text: inputValue });
-    setInputValue("");
-  };
+  // const createTodo = () => {
+  //   if (inputValue === "") return;
+  //   addTodo({ text: inputValue });
+  //   setInputValue("");
+  // };
 
   return (
     <div className="new-todo-form">
@@ -16,10 +15,13 @@ const NewTodoForm = ({ addTodo }) => {
         className="new-todo-input"
         type="text"
         placeholder="Type your new todo here..."
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        // value={inputValue}
+        // onChange={(e) => setInputValue(e.target.value)}
       />
-      <button className="new-todo-button" onClick={() => createTodo()}>
+      <button
+        className="new-todo-button"
+        // onClick={() => createTodo()}
+      >
         Create Todo
       </button>
     </div>
--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -1,48 +1,47 @@
-import { Component, createRef } from "react";
+import { Component } from "react";
 import NewTodoForm from "./NewTodoForm";
-import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoList: [],
-    };
-    this.idCounterRef = createRef(0);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     todoList: [],
+  //   };
+  //   this.idCounterRef = createRef(0);
+  // }
 
-  componentDidMount() {
-    document.title = `Todos: ${this.state.todoList.length}`;
-  }
+  // componentDidMount() {
+  //   document.title = `Todos: ${this.state.todoList.length}`;
+  // }
 
-  componentDidUpdate() {
-    document.title = `Todos: ${this.state.todoList.length}`;
-  }
+  // componentDidUpdate() {
+  //   document.title = `Todos: ${this.state.todoList.length}`;
+  // }
 
-  addTodo = (todo) => {
-    const newTodoList = this.state.todoList.concat({
-      ...todo,
-      id: this.idCounterRef.current,
-      completed: false,
-    });
-    this.setState({ todoList: newTodoList });
-    this.idCounterRef.current++;
-  };
+  // addTodo = (todo) => {
+  //   const newTodoList = this.state.todoList.concat({
+  //     ...todo,
+  //     id: this.idCounterRef.current,
+  //     completed: false,
+  //   });
+  //   this.setState({ todoList: newTodoList });
+  //   this.idCounterRef.current++;
+  // };
 
   render() {
     return (
       <div className="list-wrapper">
         <NewTodoForm addTodo={this.addTodo} />
         <div className="list-scrollable">
-          {this.state.todoList.map((todo) => (
+          {/* {this.state.todoList.map((todo) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
               removeTodo={this.removeTodo}
               editTodo={this.editTodo}
             />
-          ))}
+          ))} */}
         </div>
       </div>
     );
